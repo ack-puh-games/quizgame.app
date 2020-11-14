8981143d908d292f13b2246a54adec9092c1af5a
Austin Peterson <austin@akpwebdesign.com>
2020-11-14 20:59:05 +0000

    chore: add some basic scaffolding

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <span>This is the app.</span> 
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
