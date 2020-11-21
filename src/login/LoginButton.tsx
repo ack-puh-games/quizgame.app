e815762e9bb33e1009861207f7293dcb21bc3a53
Austin Peterson <austin@akpwebdesign.com>
2020-11-21 04:56:59 +0000

    feat: login redirects back to where you came from

--- src/login/LoginButton.tsx
+++ src/login/LoginButton.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import firebase from 'firebase/app';
 import { useAuth } from 'reactfire';
+import { useHistory } from 'react-router-dom';
+
+import useQuery from '../util/useQuery';
 
 const LoginButton: React.FC = () => {
   const auth = useAuth();
+  const history = useHistory();
+  const query = useQuery();
 
   const signIn = async () => {
     await auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+
+    history.push(query.get('from') || '/');
   };
 
   return (
--- src/util/PageWrapper.tsx
+++ src/util/PageWrapper.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { AuthCheck as RealAuthCheck, SuspenseWithPerf } from 'reactfire';
 import { Helmet } from 'react-helmet';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useLocation } from 'react-router-dom';
 
 interface PageWrapperProps {
   authCheckRequired: boolean;
@@ -12,11 +12,12 @@ interface PageWrapperProps {
 const NullAuthCheck: React.FC = ({ children }) => (children);
 
 const PageWrapper: React.FC<PageWrapperProps> = ({ authCheckRequired = true, children, title, traceId }) => {
+  const location = useLocation();
   const AuthCheck = authCheckRequired ? RealAuthCheck : NullAuthCheck;
   return (
     /* TODO: replace with actual loading component, QUIZ-12 */
     <SuspenseWithPerf fallback={<h2>Loading...</h2>} traceId={traceId}>
-      <AuthCheck fallback={<Redirect to="/login" />}>
+      <AuthCheck fallback={<Redirect to={`/login?from=${location.pathname}`} />}>
         <Helmet>
           <title>{ title }</title>
         </Helmet>
