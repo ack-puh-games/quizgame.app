1e049235c2a6659b8d795579f2ee7db0ce52e092
Austin Peterson <austin@akpwebdesign.com>
2020-12-02 06:32:57 +0000

    feat: add firebase function for new game creation tasks

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,19 +3,38 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-const firestoreDB = admin.firestore();
+const firestore = admin.firestore();
 
 export const onNewUser = functions.auth.user().onCreate((user) => {
-  return firestoreDB.collection('users')
-    .doc(user.uid)
-    .set(
-      {
-        displayName: user.displayName,
-        email: user.email,
-        creationTime: user.metadata.creationTime,
-        profileImageUrl: user.photoURL,
-
-      },
-      { merge: true }
-    );
+  return firestore.collection('users').doc(user.uid).set(
+    {
+      displayName: user.displayName,
+      email: user.email,
+      creationTime: user.metadata.creationTime,
+      profileImageUrl: user.photoURL,
+    },
+    { merge: true },
+  );
 });
+
+export const onNewGame = functions.database
+  .ref('/games/{gameId}')
+  .onCreate(async (snapshot) => {
+    const boardId = snapshot.val().board;
+    const boardRef = firestore.collection('boards').doc(boardId);
+    const questionsRef = boardRef.collection('questions');
+
+    const gameQuestionsRef = snapshot.child('questions').ref;
+
+    const questions = await questionsRef.get();
+
+    return questions.forEach(async (qDoc) => {
+      const qId = qDoc.id;
+      const qData = qDoc.data();
+
+      await gameQuestionsRef.child(qId).set({
+        categoryId: qData.categoryId,
+        value: qData.value,
+      });
+    });
+  });
